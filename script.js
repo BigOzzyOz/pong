c40b7aa6148aeb5868705595f9c5fb100a98e64e
BigOzzyOz <60309436+BigOzzyOz@users.noreply.github.com>
2024-07-07 02:01:57 +0200

    finish tutorial

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-/**
- * Description placeholder
- *
- * @param {*} muh
- */
-function test(muh) {
+let ca = document.getElementById('ca');
+let ctx = ca.getContext('2d');
+draw();
+
+function draw() {
+  ctx.fillStyle = 'white';
+  ctx.fillRect(10, 10, 10, 80);
+
+}
+
+function loop(params) {
 
 }
